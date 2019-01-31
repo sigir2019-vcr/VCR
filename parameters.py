00d4414fab0396f8af369dae9ae9e9a5446d8bc1
sigir2019-vcr <47063904+sigir2019-vcr@users.noreply.github.com>
2019-02-01 03:29:22 +0900

    Create parameters.py

---
+++ parameters.py
@@ -0,0 +1,19 @@
+# Data Path
+train_data_path = "[train_data_path]"
+test_data_path = "[validation_data_path]"
+track_data_path = "[track_data_path]"
+
+# Hyper-parameters
+embedding_dim = 25
+char_embedding_dim = 15
+genre_embedding_dim = 15
+maximum_title_length = 16
+filter_num = 15
+filter_sizes = [3,4,5]
+sample_num = 32
+epochs = 4000
+batch_size = 256
+learning_rate = 0.001
+dropout_rate = 0.5
+l2_lambda = 0.001
+candidate_num = 100
